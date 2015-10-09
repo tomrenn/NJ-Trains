25ca039d891dcb9028a2cc90dcf2366734ade925
Tom Renn <tomrenn@gmail.com>
2015-10-09 17:46:36 -0400

    Fix null pointer on restoration

--- app/src/main/java/com/tomrenn/njtrains/ui/MainFragment.java
+++ app/src/main/java/com/tomrenn/njtrains/ui/MainFragment.java
@@ -2,6 +2,7 @@ package com.tomrenn.njtrains.ui;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -142,14 +143,14 @@ public class MainFragment extends Fragment {
         super.onSaveInstanceState(outState);
     }
 
-    void restoreStops(Bundle savedInstanceState){
-        if (savedInstanceState != null){
-            fromStation = savedInstanceState.getParcelable(STATION_FROM);
+    void restoreStops(@NonNull Bundle savedInstanceState){
+        fromStation = savedInstanceState.getParcelable(STATION_FROM);
+        if (fromStation != null){
             fromStationSelection.call(fromStation);
         }
-        if (savedInstanceState != null){
-            toStation = savedInstanceState.getParcelable(STATION_TO);
-            toStationSelection.call(toStation);
+        toStation = savedInstanceState.getParcelable(STATION_TO);
+        if (toStation != null) {
+            toStationSelection.call(null);
         }
     }
 
